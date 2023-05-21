9d52c5d5f1e0b859bc8d86a6baef2af401748bc3
himkt <himkt@klis.tsukuba.ac.jp>
2023-05-22 00:04:04 +0900

    [ABC189] Solve D

---
+++ atcoder/src/bin/abc189_d.rs
@@ -0,0 +1,69 @@
+#[allow(clippy::needless_range_loop)]
+fn main() {
+    let mut scanner = Scanner::default();
+    let n: usize = scanner.cin();
+    let s: Vec<String> = scanner.vec::<String>(n);
+
+    let mut ans = 1;
+    for i in 0..n {
+        if &s[i] == "OR" {
+            ans += 2usize.pow(i as u32 + 1);
+        }
+        debug!("i={}, ans={}", i, ans);
+    }
+
+    println!("{}", ans);
+}
+
+use std::io::Write;
+pub struct Scanner {
+    buffer: std::collections::VecDeque<String>,
+    buf: String,
+}
+impl Scanner {
+    pub fn default() -> Self {
+        Scanner {
+            buffer: std::collections::VecDeque::new(),
+            buf: String::new(),
+        }
+    }
+    pub fn cin<T: std::str::FromStr>(&mut self) -> T {
+        if !self.buffer.is_empty() {
+            return self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap();
+        }
+        self.buf.truncate(0);
+        std::io::stdin().read_line(&mut self.buf).ok();
+        self.buf
+            .to_owned()
+            .split_whitespace()
+            .for_each(|x| self.buffer.push_back(String::from(x)));
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn flush(&self) {
+        std::io::stdout().flush().unwrap();
+    }
+}
+
+#[macro_export]
+macro_rules! debug {
+    () => {
+        #[cfg(debug_assertions)]
+        println!();
+    };
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        println!($($arg)*);
+    };
+}
+
+#[macro_export]
+macro_rules! ndarray {
+    // ndarray!(val; *shape)
+    ($x:expr;) => { $x };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
