efd8b20a1f1a34840259532332d1ea46187f009a
himkt <himkt@klis.tsukuba.ac.jp>
2023-06-10 13:51:07 +0900

    [ARC068] Solve A

---
+++ atcoder/src/bin/arc068_a.rs
@@ -0,0 +1,64 @@
+#[allow(clippy::needless_range_loop)]
+fn main() {
+    let mut scanner = Scanner::default();
+    let mut x: usize = scanner.cin();
+
+    let mut ans = 0;
+
+    let p = x / 11;
+    ans += 2 * p;
+    x -= 11 * p;
+
+    if x > 0 && x < 7 {
+        ans += 1;
+    }
+    if x >= 7 {
+        ans += 2;
+    }
+
+    println!("{}", ans);
+}
+
+use std::io::Write;
+pub struct Scanner {
+    buffer: std::collections::VecDeque<String>,
+    buf: String,
+}
+impl Scanner {
+    pub fn default() -> Self {
+        Scanner {
+            buffer: std::collections::VecDeque::new(),
+            buf: String::new(),
+        }
+    }
+    pub fn cin<T: std::str::FromStr>(&mut self) -> T {
+        if !self.buffer.is_empty() {
+            return self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap();
+        }
+        self.buf.truncate(0);
+        std::io::stdin().read_line(&mut self.buf).ok();
+        self.buf
+            .to_owned()
+            .split_whitespace()
+            .for_each(|x| self.buffer.push_back(String::from(x)));
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn flush(&self) {
+        std::io::stdout().flush().unwrap();
+    }
+}
+
+#[macro_export]
+macro_rules! debug {
+    () => {
+        #[cfg(debug_assertions)]
+        println!();
+    };
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        println!($($arg)*);
+    };
+}
