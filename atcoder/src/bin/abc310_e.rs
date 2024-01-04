1fea51cf33ad9979a3115750f8f68727b1294a8c
himkt <himkt@klis.tsukuba.ac.jp>
2024-01-04 23:03:04 +0900

    [ABC310] Solve E

---
+++ atcoder/src/bin/abc310_e.rs
@@ -0,0 +1,89 @@
+#[allow(clippy::needless_range_loop)]
+fn main() {
+    let mut scanner = Scanner::create();
+    let n: usize = scanner.cin();
+    let s: String = scanner.cin();
+
+    let mut a = vec![0; n];
+    for (i, si) in s.chars().enumerate() {
+        let ai: usize = si.to_digit(10).unwrap() as usize;
+        a[i] = ai;
+    }
+
+    let mut dp: Vec<Vec<usize>> = ndarray!(0; n, 2);
+    dp[0][a[0]] = 1;
+
+    fn nand(x: usize, y: usize) -> usize {
+        if x == y && x == 1 {
+            return 0;
+        }
+        1
+    }
+
+    for i in 1..n {
+        dp[i][nand(a[i], 0)] += dp[i - 1][0];
+        dp[i][nand(a[i], 1)] += dp[i - 1][1];
+        dp[i][a[i]] += 1;
+    }
+
+    let mut ans = 0;
+    for row in dp {
+        ans += row[1];
+    }
+    println!("{}", ans);
+}
+
+use std::io::Write;
+pub struct Scanner {
+    buffer: std::collections::VecDeque<String>,
+    buf: String,
+}
+impl Scanner {
+    pub fn create() -> Self {
+        Scanner {
+            buffer: std::collections::VecDeque::new(),
+            buf: String::new(),
+        }
+    }
+    pub fn cin<T: std::str::FromStr>(&mut self) -> T {
+        if !self.buffer.is_empty() {
+            return self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap();
+        }
+        self.buf.truncate(0);
+        std::io::stdin().read_line(&mut self.buf).ok();
+        self.buf
+            .to_owned()
+            .split_whitespace()
+            .for_each(|x| self.buffer.push_back(String::from(x)));
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn flush(&self) {
+        std::io::stdout().flush().unwrap();
+    }
+}
+
+#[macro_export]
+macro_rules! debug {
+    () => {
+        #[cfg(debug_assertions)]
+        println!();
+    };
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        print!("[debug] ");
+        #[cfg(debug_assertions)]
+        println!($($arg)*);
+    };
+}
+
+#[macro_export]
+macro_rules! ndarray {
+    // ndarray!(val; *shape)
+    ($x:expr;) => { $x };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
